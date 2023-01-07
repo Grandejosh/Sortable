54310f39504ab2f5abb10a83d00b07cebb98b0d6
Grandejosh <60234854+Grandejosh@users.noreply.github.com>
2023-01-07 16:47:00 -0500

    s

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -402,8 +402,8 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.35/js/uikit.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.0-beta.35/js/uikit-icons.min.js"></script>
-    @livewireStyles()
-    @livewireScripts()
+    @livewireStyles
+    @livewireScripts
 </head>
 
 <body class="antialiased">
