0214641ac39ae32736d521b1b0aa76f80f4b4cd0
Grandejosh <60234854+Grandejosh@users.noreply.github.com>
2023-01-07 17:48:54 -0500

    Update SortTest.php

--- app/Http/Livewire/SortTest.php
+++ app/Http/Livewire/SortTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire;
 
+use Exception;
 use Livewire\Component;
 use OpenAI\Laravel\Facades\OpenAI;
 
@@ -25,7 +26,7 @@ class SortTest extends Component
     public function save()
     {
         $max_tokens=1500;
-        if($this->modelo=="text-davinci-003")$max_tokens=3000;
+        if($this->modelo=="text-davinci-003")$max_tokens=3300;
 
         $this->result_text="espera el resultado...";
         if($this->parafrasear){
@@ -33,15 +34,20 @@ class SortTest extends Component
         }else{
             $consulta= $this->frase_text;
         }
-        $result = OpenAI::completions()->create([
-            'model' => $this->modelo,
-            'prompt' => $consulta,
-            'max_tokens' => $max_tokens,
-        ]);
-        $this->result_text = $result['choices'][0]['text'];
-        $this->query_tokens = $result['usage']['prompt_tokens'];
-        $this->result_tokens = $result['usage']['completion_tokens'];
-        $this->consumed_tokens = $result['usage']['total_tokens'];
+        try{
+            $result = OpenAI::completions()->create([
+                'model' => $this->modelo,
+                'prompt' => $consulta,
+                'max_tokens' => $max_tokens,
+            ]);
+            $this->result_text = $result['choices'][0]['text'];
+            $this->query_tokens = $result['usage']['prompt_tokens'];
+            $this->result_tokens = $result['usage']['completion_tokens'];
+            $this->consumed_tokens = $result['usage']['total_tokens'];
+        }catch(Exception $e){
+            $this->result_text=$e->getMessage();
+        }
+
     }
     public function alert(){
         echo"<script>alert('carajo');</script>";
